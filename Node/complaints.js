e17ad64134102d153a6751d3e42853fbd279e800
EyalMorad <72889776+EyalMorad@users.noreply.github.com>
2020-11-16 23:56:26 +0200

    Add files via upload

---
+++ Node/complaints.js
@@ -0,0 +1,24 @@
+
+const consts2 = require("./consts")
+const complaintsHandler = require("./complaintsHandler")
+
+
+
+let complaint1 = {
+    text: "I'm not getting enough money",
+    type: consts2.obj.FINANCE
+}
+
+let complaint2 = {
+    text: "My salary hasn't come in yet",
+    type: consts2.obj.FINANCE
+}
+
+let complaint3 = {
+    text: "I'm always full of energy",
+    type: consts2.obj.EMOTIONS
+}
+
+complaintsHandler.handleComplaints(complaint1) //should print "Money doesn't grow on trees, you know."
+complaintsHandler.handleComplaints(complaint2) //should print "Money doesn't grow on trees, you know."
+complaintsHandler.handleComplaints(complaint3) //should print "It'll pass, as all things do, with time."
---
+++ Node/consts.js
@@ -0,0 +1,13 @@
+
+const FINANCE = "finance"
+const WEATHER = "weather"
+const EMOTIONS = "emotions"
+
+
+const obj = {
+    FINANCE : "finance",
+    WEATHER : "weather",
+    EMOTIONS : "emotions"
+}
+
+module.exports.obj = obj
